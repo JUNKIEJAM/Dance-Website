4cc7676319f6c57cc1c87f226daa210a1098c0fb
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-01-29 00:03:52 +0530

    Front end start

---
+++ app.js
@@ -0,0 +1,33 @@
+const express=require("express");
+const path=require("path")
+const app=express();
+const fs=require("fs");
+const port=333;
+
+//EXPRESS SPECIFIC STUFF
+//serving static files
+app.use('/static',express.static('static'));
+app.use(express.urlencoded());
+
+// set the template engine as pug
+
+app.set('view engine','pug');
+
+//PUG Specific Stuf
+//set the view directory
+app.set('views',path.join(__dirname,'views'));
+
+
+//END POINTS
+app.get('/',(req,res)=>{
+     
+   // const con="This is the best content on website";
+    const params={'title':'This is the best website'};
+    res.status(200).render('index.pug',params)    // render for template and not send
+})
+
+
+//Start the server
+app.listen(port,()=>{
+    console.log(`This application started successfully on port ${port}`);
+});
