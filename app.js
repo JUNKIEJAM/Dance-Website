78f6a03c20ec7049c0a1661b06b7f6401ea8e38e
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-02-02 23:32:05 +0530

    commit

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ const path=require("path")
 const app=express();
 //const fs=require("fs");
 const port=333;
+const bodyParser=require('body-parser');
+const mongoose=require('mongoose');
+
+
+//define mongoose schema
+var contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    address: String,
+    desc: String
+    
+    
+  });
+
+  var Contact = mongoose.model('Contact', contactSchema );
 
 //EXPRESS SPECIFIC STUFF
 //serving static files
 app.use('/static',express.static('static'));
 app.use(express.urlencoded());
+//app.use(bodyParser.urlencoded());
+
+//app.use(bodyParser.json());
 
 // set the template engine as pug
 
@@ -31,6 +50,18 @@ app.get('/contact',(req,res)=>{
     // const con="This is the best content on website";
      const params={};
      res.status(200).render('contact.pug',params)    // render for template and not send
+ })
+ 
+ app.post('/contact',(req,res)=>{
+     
+    var myData=new Contact(req.body);
+    myData.save().then(()=>{    //asynchronous
+           res.send("This item has been saved in the database")
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database");
+    });
+
+   // res.status(200).render('contact.pug')    // render for template and not send
  })
  
 
